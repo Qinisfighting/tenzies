056e5914f984a478326cbf73fb13396d7663de76
Qinisfighting <yq.qualmann@gmail.com>
2023-04-08 07:07:26 +0200

    waiting for one more feather

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from "react"
+import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
+import Die from "./components/Die"
+
 
 function App() {
+  const [newDiceArray, setNewDiceArray] = React.useState(allNewDice())
+  const [tenzies, setTenzies] = React.useState(false)
+  const diceElements = newDiceArray.map(die => 
+    <Die key={die.id}
+         value={die.value}
+         index={newDiceArray.indexOf(die)}
+         isHeld={die.isHeld}
+         onclick={() => holdDice(die.id)} 
+    /> 
+    )
+ 
+
+   React.useEffect(() => {
+      const allHeld = newDiceArray.every(die => die.isHeld)
+      const winValue = newDiceArray[0].value
+      const allSameValue = newDiceArray.every(die => die.value === winValue)
+      if(allHeld && allSameValue){
+        setTenzies(true)
+        console.log("You won!")
+      }
+}, [newDiceArray]) 
+    
+    if(tenzies === true){
+
+    }
+
+
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    } 
+
+    function allNewDice() {
+      const nrArray = []
+      for (let i = 0; i < 10; i++) {
+        nrArray.push(generateNewDie())
+      }
+      return nrArray
+    }
+
+
+
+    function holdDice(id) {
+     setNewDiceArray(preDices => preDices.map(die => {
+        return die.id === id ? {
+          ...die,
+          isHeld: !die.isHeld
+        } : die;
+      })
+      )
+      
+    }
+
+
+    function rollDice() {
+      
+      if(!tenzies){setNewDiceArray(oldDices => oldDices.map(die => {
+        return die.isHeld ? 
+          die :
+          generateNewDie()
+      }))
+    } else {
+      setTenzies(false)
+      setNewDiceArray(allNewDice())
+    }
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className='confetti'>{tenzies && <Confetti />}</div>
+      <h1 className="title">Tenzies</h1>
+      <hr/>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className='container'>
+        {diceElements}
+      </div>
+      
+      <button 
+        className="roll-dice" 
+        onClick={rollDice}>{tenzies ? "New Game" : "Roll"}
+      </button>
+      
+                  
+           
+        
+    </main>
   );
 }
-
 export default App;
---
+++ src/components/Die.js
@@ -0,0 +1,88 @@
+import '../App.css';
+import React from "react"
+
+function Die(props) {
+
+    function styles() {
+      if (props.isHeld === false) {
+        if (props.index === 3 || props.index === 6) {
+          return "one item"
+        }
+        if (props.index === 1 || props.index === 8) {
+          return "two item"
+        }
+        if (props.index === 4 || props.index === 7) {
+          return "three item"
+        }
+        if (props.index === 0 || props.index === 9) {
+          return "four item"
+        }
+        if (props.index === 2 || props.index === 5) {
+          return "five item"
+        }
+      } else {
+        return "held"
+      }
+    
+  }
+  /**/
+    function diceStyles(props){
+       
+      if(props.value === 1){
+        return <div className="face">
+                 <span className="pip" style={{marginTop: 14}}></span>        
+               </div>
+      }
+      if(props.value === 2){
+        return <div className="face">
+                 <span className="pip"></span>
+                 <span className="pip" style={{marginTop: 14}}></span>   
+               </div>
+      }
+      if(props.value === 3){
+        return <div className="face">
+                 <span className="pip"></span>
+                 <span className="pip"></span>
+                 <span className="pip"></span>
+                
+               </div>
+      }
+      if(props.value === 4){
+         return <div className="face">
+                 <span className="pip" style={{marginTop: 2}}></span>
+                 <span className="pip" style={{marginTop: 10}}></span>
+                 <span className="pip" style={{marginTop: 2}}></span>
+                 <span className="pip" style={{marginTop: 10}}></span>
+               
+               </div>
+      }
+      if(props.value === 5){
+        return <div className="face">
+               <span className="pip"></span>
+               <span className="pip"></span>  
+               <span className="pip"></span>  
+               <span className="pip"></span>  
+               <span className="pip"></span>          
+            </div>
+      }
+      if(props.value === 6){
+        return <div className="face">
+              <span className="pip"></span>
+              <span className="pip"></span>  
+              <span className="pip"></span>  
+              <span className="pip"></span>  
+              <span className="pip"></span>  
+              <span className="pip"></span>          
+     </div>
+    }
+  }
+    return (
+      <div className={styles()} onClick={props.onclick}> {diceStyles(props)}
+        {/* <h3 className={styles()} onClick={props.onclick}>{props.value}</h3> */ }  
+      </div>
+    )
+    
+  }
+  
+
+  export default Die
