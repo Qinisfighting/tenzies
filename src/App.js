eefd7efb0b37a4eb01603378a9bf1e3b853279a7
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-12-31 13:26:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-
-import './App.css';
-import React from "react"
-import {nanoid} from "nanoid"
-import Confetti from "react-confetti"
-import Die from "./components/Die"
-
-
+import "./App.css";
+import React from "react";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
+import Die from "./components/Die";
 
 function App() {
-  const [newDiceArray, setNewDiceArray] = React.useState(allNewDice())
-  const [tenzies, setTenzies] = React.useState(false)
+  const [newDiceArray, setNewDiceArray] = React.useState(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
   const [time, setTime] = React.useState(0);
   const [isRunning, setIsRunning] = React.useState(false);
-  const [records, setRecords] = React.useState(() => JSON.parse(localStorage.getItem("records")) || []) 
-  const [currentRecord, setCurrentRecord] = React.useState(Math.min(...records) || "")
-   
+  const [records, setRecords] = React.useState(
+    () => JSON.parse(localStorage.getItem("records")) || []
+  );
+  const [currentRecord, setCurrentRecord] = React.useState(
+    Math.min(...records) || ""
+  );
 
   React.useEffect(() => {
-      localStorage.setItem("records", JSON.stringify(records))
-}, [records])
-
+    localStorage.setItem("records", JSON.stringify(records));
+  }, [records]);
 
   React.useEffect(() => {
     let intervalId;
     if (isRunning) {
-     //setting time from 0 to 1 every 10 milisecond 
+      //setting time from 0 to 1 every 10 milisecond
       intervalId = setInterval(() => setTime(time + 1), 10);
     }
     //stop the interval when the component unmounts
@@ -32,27 +31,30 @@ function App() {
   }, [isRunning, time]);
 
   React.useEffect(() => {
-    const allHeld = newDiceArray.every(die => die.isHeld)
-    const winValue = newDiceArray[0].value
-    const allSameValue = newDiceArray.every(die => die.value === winValue)
-    if(allHeld && allSameValue){
-      setTenzies(true)
-      setIsRunning(false)
+    const allHeld = newDiceArray.every((die) => die.isHeld);
+    const winValue = newDiceArray[0].value;
+    const allSameValue = newDiceArray.every((die) => die.value === winValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+      setIsRunning(false);
     }
-}, [newDiceArray]) 
+  }, [newDiceArray]);
 
-  const stopWatch = () => { 
-    const minutes = Math.floor((time % 360000) / 6000); 
+  const stopWatch = () => {
+    const minutes = Math.floor((time % 360000) / 6000);
     const seconds = Math.floor((time % 6000) / 100);
-    const milliseconds = time % 100; 
-    const timeScore = milliseconds >= 10 ? `${seconds}.${milliseconds}` : `${seconds}.0${milliseconds}`
-    if(tenzies){
-      if(timeScore < Math.min(...records)){
-      setCurrentRecord(timeScore) 
-      records.push(currentRecord)
-      setRecords(prevRecords => [...prevRecords, timeScore])
-    }}
-    
+    const milliseconds = time % 100;
+    const timeScore =
+      milliseconds >= 10
+        ? `${seconds}.${milliseconds}`
+        : `${seconds}.0${milliseconds}`;
+    if (tenzies) {
+      if (timeScore < Math.min(...records)) {
+        setCurrentRecord(timeScore);
+        records.push(currentRecord);
+        setRecords((prevRecords) => [...prevRecords, timeScore]);
+      }
+    }
 
     //console.log(records)
     //console.log(time)
@@ -68,102 +70,121 @@ function App() {
         </p>
       </div>
     );
+  };
+
+  const clearRecord = () => {
+    window.location.reload(false);
+    localStorage.clear();
+  };
+
+  const diceElements = newDiceArray.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      index={newDiceArray.indexOf(die)}
+      isHeld={die.isHeld}
+      onclick={() => holdDice(die.id)}
+    />
+  ));
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
   }
 
-  const clearRecord = ()=> {
-    window.location.reload(false)
-    localStorage.clear()
-  }
- 
-
-  const diceElements = newDiceArray.map(die => 
-    <Die key={die.id}
-         value={die.value}
-         index={newDiceArray.indexOf(die)}
-         isHeld={die.isHeld}
-         onclick={() => holdDice(die.id)} 
-    /> 
-    )
- 
-    function generateNewDie() {
-        return {
-            value: Math.ceil(Math.random() * 6),
-            isHeld: false,
-            id: nanoid()
-        }
-    } 
-
-    function allNewDice() {
-      
-      const nrArray = []
-      for (let i = 0; i < 10; i++) {
-        nrArray.push(generateNewDie())
-      }
-      return nrArray
+  function allNewDice() {
+    const nrArray = [];
+    for (let i = 0; i < 10; i++) {
+      nrArray.push(generateNewDie());
     }
+    return nrArray;
+  }
 
-    function holdDice(id) {
-     setNewDiceArray(preDices => preDices.map(die => {
-        return die.id === id ? {
-          ...die,
-          isHeld: !die.isHeld
-        } : die;
+  function holdDice(id) {
+    setNewDiceArray((preDices) =>
+      preDices.map((die) => {
+        return die.id === id
+          ? {
+              ...die,
+              isHeld: !die.isHeld,
+            }
+          : die;
       })
-      )
-      
-    }
+    );
+  }
 
-    function rollDice() {
-
-      setIsRunning(true)
-      if(!tenzies){setNewDiceArray(oldDices => oldDices.map(die => {
-        return die.isHeld ? 
-          die :
-          generateNewDie()
-             
-      } 
-      ))
+  function rollDice() {
+    setIsRunning(true);
+    if (!tenzies) {
+      setNewDiceArray((oldDices) =>
+        oldDices.map((die) => {
+          return die.isHeld ? die : generateNewDie();
+        })
+      );
     } else {
-      setIsRunning(false)
-      setTenzies(false)
-      setNewDiceArray(allNewDice())
-      setTime(0)
-      stopWatch()
+      setIsRunning(false);
+      setTenzies(false);
+      setNewDiceArray(allNewDice());
+      setTime(0);
+      stopWatch();
     }
   }
-    
+
   return (
     <main>
-      <div className='confetti'>{tenzies && <Confetti />}</div>
+      <div className="confetti">{tenzies && <Confetti />}</div>
       <h1 className="title">Tenzies</h1>
-      <hr/>
-      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-      <div className='container'>
-        {diceElements}
-      </div>
-      
-      <button 
-        className="roll-dice" 
-        onClick={rollDice}>{tenzies ? "New Game" : "Roll"}
+      <hr />
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="container">{diceElements}</div>
+
+      <button className="roll-dice" onClick={rollDice}>
+        {tenzies ? "New Game" : "Roll"}
       </button>
-      <div className='stopwatch'>{stopWatch()}</div> 
-      
-      <p className='record'   >Current Record: <i style={{color: 'rgb(172, 78, 78)', fontWeight: 'bold'}}>{records[records.length - 1]} </i>s
-                                       <button className='clear' 
-                                            style={{
-                                            fontSize: 12, 
-                                            padding: 0, 
-                                            width: 80, 
-                                            height: 20, 
-                                            marginLeft: 10,
-                                            background: 'rgba(87, 87, 87, 0.7)',
-                                            color: 'white'
-                                            }} 
-                                            onClick={() =>{clearRecord()}}>clear record
-                                       </button>
-      </p>  
-      <hr/>                  
-      <p style = {{fontSize: 10, margin: '5px auto 10px'}}>QIN勤©2023 up&qu</p>    
+      <div className="stopwatch">{stopWatch()}</div>
+
+      <p className="record">
+        Current Record:{" "}
+        <i style={{ color: "rgb(172, 78, 78)", fontWeight: "bold" }}>
+          {records[records.length - 1]}{" "}
+        </i>
+        s
+        <button
+          className="clear"
+          style={{
+            fontSize: 12,
+            padding: 0,
+            width: 80,
+            height: 20,
+            marginLeft: 10,
+            background: "rgba(87, 87, 87, 0.7)",
+            color: "white",
+          }}
+          onClick={() => {
+            clearRecord();
+          }}
+        >
+          clear record
+        </button>
+      </p>
+      <hr />
+      <p style={{ fontSize: 10, margin: "5px auto 10px" }}>
+        {" "}
+        © {new Date().getFullYear()} by{" "}
+        <a
+          href="https://www.yanqin.de"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          QIN's code
+        </a>
+      </p>
     </main>
   );
 }
